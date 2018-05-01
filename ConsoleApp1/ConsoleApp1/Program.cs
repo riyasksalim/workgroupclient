85db26c715119080b7c50a560409340e9a543c99
mujeebks <mujeebks@gmail.com>
2018-05-01 11:16:53 +0530

    Console Application

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassSearchCriteria sc = new ClassSearchCriteria();
+            //var searchCriteria = sc.GetSearchCriteria();
+            var dateFrom = Convert.ToDateTime("2013-06-06 13:47:45.000");
+            var dateTo = Convert.ToDateTime("2013-06-06 13:55:24.000");
+            string workgroupid = null;
+            List<GetReportDailyJob_Result> studentInfos = sc.GetValues(dateFrom, dateTo, workgroupid);
+            List<ReportInfo> lstReportInfo = new List<ReportInfo>();
+            ReportInfo reportInfo = null;
+            foreach (var s in studentInfos)
+            {
+                reportInfo = new ReportInfo
+                {
+                    starttime = s.starttime,
+                    endtime = s.endtime,
+                    workgroupid = s.workgroupid.ToString(),
+                    mediaid = s.mediaid.ToString(),
+                    dnis = s.dnis,
+                    ani = s.ani,
+                    updateuserid = s.updateuserid.ToString(),
+                    percentscore = s.percentscore,
+                    overallscore = s.overallscore,
+                    reviewdate = s.reviewdate,
+                    username = s.username,
+                    userroleid = s.userroleid,
+                    usertypeid = s.usertypeid,
+                    workgroupname = s.workgroupname,
+                    description = s.description,
+                    name = s.name,
+                    sequencenumber = s.sequencenumber,
+                    weight = s.weight,
+                    questiondescription = s.questiondescription,
+                    questionnumber = s.questionnumber,
+                    questiontext = s.questiontext,
+                    responserequired = s.responserequired,
+                    questionadditionalpoint = s.questionadditionalpoint,
+                    autofailpoint = s.autofailpoint,
+                    questionadditionalconditionpoint = s.questionadditionalconditionpoint
+                };
+                lstReportInfo.Add(reportInfo);
+            }
+            sc.CreateCSV(lstReportInfo);
+            //UpdateSearchCriteria();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/ReportInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ReportInfo
+    {
+        public string workgroupid { get; set; }
+        public DateTime? starttime { get; set; }
+        public DateTime? endtime { get; set; }
+        public string mediaid { get; set; }
+        public string dnis { get; set; }
+        public string ani { get; set; }
+        public string updateuserid { get; set; }
+        public Int16? percentscore { get; set; }
+        public string overallscore { get; set; }
+        public DateTime? reviewdate { get; set; }
+        public string username { get; set; }
+        public Int16 userroleid { get; set; }
+        public Int16 usertypeid { get; set; }
+        public string workgroupname { get; set; }
+        public string description { get; set; }
+        public string name { get; set; }
+        public Int16 sequencenumber { get; set; }
+        public Int16 weight { get; set; }
+        public string questiondescription { get; set; }
+        public Int16 questionnumber { get; set; }
+        public string questiontext { get; set; }
+        public bool responserequired { get; set; }
+        public decimal? questionadditionalpoint { get; set; }
+        public decimal? autofailpoint { get; set; }
+        public decimal? questionadditionalconditionpoint { get; set; }
+
+    }
+}
