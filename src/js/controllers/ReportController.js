d37dcb256f23c3fa8657e21581c5b96ae0e7604e
riyas <riyasalim123@gmail.com>
2018-05-02 13:35:41 -0700

    dev13

--- src/js/controllers/ReportController.js
+++ src/js/controllers/ReportController.js
@@ -4,7 +4,73 @@
     $scope.itemsByPage=15
     var baseUrl = ApiUrl;
     $scope.loading = false;
-   
+    var settings = {
+        'toaster':
+        {
+            'id': 'toaster',
+            'container': 'body',
+            'template': '<div></div>',
+            'class': 'toaster',
+            'css':
+            {
+                'position': 'fixed',
+                'top': '10px',
+                'right': '10px',
+                'width': '300px',
+                'zIndex': 50000
+            }
+        },
+
+        'toast':
+        {
+            'template':
+            '<div class="alert alert-%priority% alert-dismissible" role="alert">' +
+                '<button type="button" class="close" data-dismiss="alert">' +
+                    '<span aria-hidden="true">&times;</span>' +
+                    '<span class="sr-only">Close</span>' +
+                '</button>' +
+                '<span class="title"></span>: <span class="message"></span>' +
+            '</div>',
+
+            'css': {},
+            'cssm': {},
+            'csst': { 'fontWeight': 'bold' },
+
+            'fade': 'slow',
+
+            'display': function ($toast) {
+                return $toast.fadeIn(settings.toast.fade);
+            },
+
+            'remove': function ($toast, callback) {
+                return $toast.animate(
+                    {
+                        opacity: '0',
+                        padding: '0px',
+                        margin: '0px',
+                        height: '0px'
+                    },
+                    {
+                        duration: settings.toast.fade,
+                        complete: callback
+                    }
+                );
+            }
+        },
+
+        'debug': false,
+        'timeout': 5500,
+        'stylesheet': null,
+        'donotdismiss': []
+    };
+
+    $.toaster({
+        settings:settings,
+        message: 'Your message hereqqqqqqqqqqq',
+        title: 'Yor title'
+
+    });
+
     $(function () {
         $("#StartDate").datepicker({
             onSelect: function (date) {
