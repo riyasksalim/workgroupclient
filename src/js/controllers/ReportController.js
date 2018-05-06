f9385fc14538942e9266a4981c3dd78a527e79ad
riyas <riyasalim123@gmail.com>
2018-05-06 02:45:20 -0700

    dev26

--- src/js/controllers/ReportController.js
+++ src/js/controllers/ReportController.js
@@ -106,7 +106,7 @@
         var params = {
             StartDate: $scope.StartDate,
             EndDate: $scope.EndDate,
-            WorkGroupID: $scope.workgroupselected.WorkGroupId
+            WorkGroupID: $scope.workgroupselected
         };
 
         (httpRequest = dataService.GetWorkGroupReport(params).then(function (response) {
@@ -131,43 +131,36 @@
     };
 
 
-    $scope.noResultsTag = null;
-    $scope.tags = [
-        {id: 0, name: "Zero"},
-        {id: 1, name: "One"},
-        {id: 2, name: "Two"}, 
-        {id: 3, name: "Three"}, 
-        {id: 4, name: "Four"}, 
-    ];
+   
     $scope.select2Options = {
-        formatNoMatches: function(term) {
-            console.log("Term: " + term);
-            var message = '<a ng-click="addTag()">Add tag:"' + term + '"</a>';
-            if(!$scope.$$phase) {
-                $scope.$apply(function() {
-                    $scope.noResultsTag = term;
-                });
-            }
-            return message;
-        }
+        // formatNoMatches: function(term) {
+           
+        //     var message = '<a ng-click="addTag()">Add tag:"' + term + '"</a>';
+        //     if(!$scope.$$phase) {
+        //         $scope.$apply(function() {
+        //             $scope.noResultsTag = term;
+        //         });
+        //     }
+        //     return message;
+        // }
     };
     
-    $scope.addTag = function() {
-        $scope.tags.push({
-            id: $scope.tags.length,
-            name: $scope.noResultsTag
-        });
-    };
+    // $scope.addTag = function() {
+    //     $scope.tags.push({
+    //         id: $scope.tags.length,
+    //         name: $scope.noResultsTag
+    //     });
+    // };
     
-    $scope.$watch('noResultsTag', function(newVal, oldVal) {
-        if(newVal && newVal !== oldVal) {
-            $timeout(function() {
-                var noResultsLink = $('.select2-no-results');
-                console.log(noResultsLink.contents());
-                $compile(noResultsLink.contents())($scope);
-            });
-        }
-    }, true);
+    // $scope.$watch('noResultsTag', function(newVal, oldVal) {
+    //     if(newVal && newVal !== oldVal) {
+    //         $timeout(function() {
+    //             var noResultsLink = $('.select2-no-results');
+    //             console.log(noResultsLink.contents());
+    //             $compile(noResultsLink.contents())($scope);
+    //         });
+    //     }
+    // }, true);
 
 
 
