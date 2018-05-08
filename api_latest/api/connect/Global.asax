68e72f6a863f7fff4472cec56d52b3ad6c324db8
KS <Mujeeb.KS@shell.com>
2018-05-08 18:30:47 +0530

    server api files latest

---
+++ api_latest/api/connect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="connect.WebApiApplication" Language="C#" %>
---
+++ api_latest/api/connect/Models/WorkGroupReportBO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace connect.Models
+{
+    public class WorkGroupReportBO
+    {
+        public string workgroupid { get; set; }
+
+        public DateTime? starttime { get; set; }
+        public DateTime? endtime { get; set; }
+        public Guid? mediaid { get; set; }
+        public string dnis { get; set; }
+        public string ani { get; set; }
+        public Guid? updateuserid { get; set; }
+        public short? percentscore { get; set; }
+        public string overallscore { get; set; }
+        public DateTime? reviewdate { get; set; }
+        public string username { get; set; }
+        public byte userroleid { get; set; }
+        public byte usertypeid { get; set; }
+        public string workgroupname { get; set; }
+        public string description { get; set; }
+        public string name { get; set; }
+        public byte sequencenumber { get; set; }
+        public byte sweight { get; set; }
+        public byte sweight1 { get; set; }
+        public string questiondescription { get; set; }
+        public byte questionnumber { get; set; }
+        public string questiontext { get; set; }
+        public byte qweight { get; set; }
+        public bool responserequired { get; set; }
+        public decimal? questionadditionalpoint { get; set; }
+        public decimal? autofailpoint { get; set; }
+        public decimal? questionadditionalconditionpoint { get; set; }
+        public double? weightedscore { get; set; }
+        public byte sectionWeight { get; set; }
+        public string responsetext { get; set; }
+        public byte questionWeight { get; set; }
+        public string questiontypedesc { get; set; }
+        public bool questionScored { get; set; }
+    }
+}
