0494eeb67ba3212bc9cbdb3e5eb92e806667031f
Riyas-KS <riyasks@vofoxsolutions.com>
2018-05-16 11:55:56 +0530

    f

--- client/src/js/controllers/ReportController.js
+++ client/src/js/controllers/ReportController.js
@@ -132,7 +132,7 @@
     }
     function checkparams() {
         debugger
-        if ($scope.StartDate == undefined || $scope.StartDate == "" || $scope.EndDate == undefined || $scope.StartDate == "" || $scope.workgroupselected == undefined) {
+        if ($scope.StartDate == undefined || $scope.StartDate == "" || $scope.EndDate == undefined || $scope.StartDate == "") {
 
             $.toaster({
                 settings: settings,
@@ -160,12 +160,31 @@
         }
     }
     function checkdateformat(start,end){
-        var startdate=Date.parse(start);
-        var enddate=Date.parse(end);
-        var a= (new Date(start) !== "Invalid Date") && !isNaN(new Date(start));
-        var b= (new Date(end) !== "Invalid Date") && !isNaN(new Date(end));
-        debugger;
+        debugger
+        var startdate=validate(start);
+        var enddate=validate(end);
+        if(startdate && enddate){
+            return true;
+        }
+        else{
+            return false;
+        }
+     
     }
+   function validate(text){
+   // var text = '30-05-2015';
+    var comp = text.split('-');
+    var m = parseInt(comp[1], 10);
+    var d = parseInt(comp[0], 10);
+    var y = parseInt(comp[2], 10);
+    var date = new Date(y,m-1,d);
+    if (date.getFullYear() == y && date.getMonth() + 1 == m && date.getDate() == d) {
+       return true;
+    } else {
+      return false;
+    }
+
+   }
     $scope.$watch('StartDate', function(newValue, oldValue) {
         console.log(newValue);
       $scope.StartDate=newValue
