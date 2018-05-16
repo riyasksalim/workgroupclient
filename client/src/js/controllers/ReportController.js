fdaa3d6e27d095422b13fbbf31b7270ab3acbdf8
Riyas-KS <riyasks@vofoxsolutions.com>
2018-05-16 14:11:05 +0530

    qdswd

--- client/src/js/controllers/ReportController.js
+++ client/src/js/controllers/ReportController.js
@@ -174,8 +174,8 @@
    function validate(text){
    // var text = '30-05-2015';
     var comp = text.split('-');
-    var m = parseInt(comp[1], 10);
-    var d = parseInt(comp[0], 10);
+    var m = parseInt(comp[0], 10);
+    var d = parseInt(comp[1], 10);
     var y = parseInt(comp[2], 10);
     var date = new Date(y,m-1,d);
     if (date.getFullYear() == y && date.getMonth() + 1 == m && date.getDate() == d) {
