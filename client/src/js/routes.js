0119c149312d33676a74fdeb2026ef50caf34801
riyas <riyasalim123@gmail.com>
2018-06-20 20:49:47 +0530

    fixed design

--- client/src/js/routes.js
+++ client/src/js/routes.js
@@ -12,7 +12,7 @@ angular
   $breadcrumbProvider.setOptions({
     prefixStateName: 'app.main',
     includeAbstract: true,
-    template: '<li class="breadcrumb-item" ng-repeat="step in steps" ng-class="{active: $last}" ng-switch="$last || !!step.abstract"><a ng-switch-when="false" href="{{step.ncyBreadcrumbLink}}">{{step.ncyBreadcrumbLabel}}</a><span ng-switch-when="true">{{step.ncyBreadcrumbLabel}}</span></li>'
+    template: '<li class="breadcrumb-item" ng-repeat="step in steps" ng-class="{active: $last}" ng-switch="$last || !!step.abstract"><a ng-switch-when="false" href="#/components/list">{{step.ncyBreadcrumbLabel}}</a><span ng-switch-when="true">{{step.ncyBreadcrumbLabel}}</span></li>'
   });
 
   $stateProvider
@@ -54,37 +54,37 @@ angular
       }],
     }
   })
-  // .state('app.main', {
-  //   url: '/dashboard',
-  //   templateUrl: 'views/main.html',
-  //   //page title goes here
-  //   ncyBreadcrumb: {
-  //     label: 'Home',
-  //   },
-  //   //page subtitle goes here
-  //   params: { subtitle: 'Welcome to ROOT powerfull Bootstrap & AngularJS UI Kit' },
-  //   resolve: {
-  //     loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {
-  //       // you can lazy load files for an existing module
-  //       //return $ocLazyLoad.load([
-  //       //  {
-  //       //    serie: true,
-  //       //    name: 'chart.js',
-  //       //    files: [
-  //       //      'node_modules/chart.js/dist/Chart.min.js',
-  //       //      'node_modules/angular-chart.js/dist/angular-chart.min.js'
-  //       //    ]
-  //       //  },
-  //       //]);
-  //     }],
-  //     loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
-  //       // you can lazy load controllers
-  //       //return $ocLazyLoad.load({
-  //       //  files: ['js/controllers/main.js']
-  //       //});
-  //     }]
-  //   }
-  // })
+  .state('app.main', {
+    url: '/dashboard',
+    templateUrl: 'views/main.html',
+    //page title goes here
+    ncyBreadcrumb: {
+      label: 'Home',
+    },
+    //page subtitle goes here
+    params: { subtitle: 'Welcome to ROOT powerfull Bootstrap & AngularJS UI Kit' },
+    resolve: {
+      loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {
+        // you can lazy load files for an existing module
+        //return $ocLazyLoad.load([
+        //  {
+        //    serie: true,
+        //    name: 'chart.js',
+        //    files: [
+        //      'node_modules/chart.js/dist/Chart.min.js',
+        //      'node_modules/angular-chart.js/dist/angular-chart.min.js'
+        //    ]
+        //  },
+        //]);
+      }],
+      loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+        // you can lazy load controllers
+        //return $ocLazyLoad.load({
+        //  files: ['js/controllers/main.js']
+        //});
+      }]
+    }
+  })
   .state('appSimple', {
     abstract: true,
     templateUrl: 'views/common/layouts/simple.html',
